0f1cda6b297af51bc3b04bb81d19466d02aef9e7
Lucas <lucaspdroz@gmail.com>
2020-08-15 16:45:22 -0300

    feat: adding colors and themes

--- src/components/StyleBase/index.tsx
+++ src/components/StyleBase/index.tsx
@@ -3,7 +3,18 @@ import { createGlobalStyle } from 'styled-components';
 const BaseTheme = createGlobalStyle`
     :root{
         /* add here all colors and dimensions */
+        --primary-dark:#1F2326;
+        --secoundary-dark:#494B56;
+        --light-dark:#888D97;
+
+        --light-blue:#57A2ED;
+        --light-cian:#6CD9E9;
+
+        --light-yellow:#F8CB84;
+
+        --light-red:#FE7E7C;
     }
+
     * {
         margin: 0;
         padding: 0;
