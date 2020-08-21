b84e2aae4140643fdba2bcb449fde2b0c3327f02
githiago-f <52945005+githiago-f@users.noreply.github.com>
2020-08-21 01:25:10 +0000

    Deploying to gh-pages from  @ 340b4e09f4d4abb1574ba3c84e73b46978c41007 🚀

---
+++ precache-manifest.28e1ab89e2aa75d81b561caba6945989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a38e7776cc2f308cd6134453fd0d70",
+    "url": "/digital-wallet/index.html"
+  },
+  {
+    "revision": "79efb0ab7ed612af0fe5",
+    "url": "/digital-wallet/static/js/2.cd50b5d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/digital-wallet/static/js/2.cd50b5d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7df712acd9101a3d9f4",
+    "url": "/digital-wallet/static/js/main.73e222f8.chunk.js"
+  },
+  {
+    "revision": "e605d50c50dcc5da4200",
+    "url": "/digital-wallet/static/js/runtime-main.847f7b05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.719da9af5d0ac4113c8042404fc1ff35.js"
+  "/digital-wallet/precache-manifest.28e1ab89e2aa75d81b561caba6945989.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/digital-wallet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
