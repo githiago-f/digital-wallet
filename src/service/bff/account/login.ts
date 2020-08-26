e022a8a72a3fad7ce204c0379e329ed264c55b99
Nikolas Freitas <nikolas.freitas@poatek.com>
2020-08-26 00:23:49 -0300

    :sparkles: Create base of service to consume apis

---
+++ src/service/bff/account/login.ts
@@ -0,0 +1,19 @@
+import { AxiosInstance } from 'axios';
+import { RequestObject } from '../../types';
+
+export interface LoginResponse {
+  token: string;
+}
+
+export interface BffIntegration extends RequestObject<BffIntegration> {
+  login: (account: string, password: string) => Promise<LoginResponse>;
+}
+
+const routesInstances = (axios: AxiosInstance): BffIntegration => ({
+    login: (account: string, password: string) => axios.post('/authenticate', {
+        account,
+        password,
+    }),
+});
+
+export default routesInstances;
